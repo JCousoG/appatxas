c77bd2c8e69506cff065812650aef23cd9891fb5
Couso <jcousog@hotmail.com>
2023-03-03 20:30:56 +0100

    componente total

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import Saldo from "./componentes/saldo";
+
 import "./App.css"
+import { useState } from "react";
 
 
 
 function App() {
-  return (
-    
+
+ let [total, setTotal] = useState("")
+ let [numero, setNumero] = useState(0)
+ function manexadorTotal(evento) {
+  setNumero( parseFloat (evento.target.value))
+  if (isNaN(numero)){
+    setTotal("");
+    setNumero(0);}
+    else
+  setTotal(evento.target.value) 
+ }
+ return (
+      <>
+    <h1>APPATXAS</h1>
+          <label>
+        Total:
+        <input type="text" value={total} onInput={manexadorTotal}/>
+      </label>
+        <p> {numero} </p>
+    </>
   );
 }
 
