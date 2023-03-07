aa9181f2499a1e9f2e658dd7cb3c1a13b06ebc8b
Vero-SB <verosb.dev@gmail.com>
2023-03-07 19:10:36 +0100

    Engadimos Aportacion.jsx

---
+++ src/componentes/Aportacion.jsx
@@ -0,0 +1,44 @@
+
+import React from "react";
+import {useState} from "react";
+import customer from "../customer.png";
+import Saldo from "./"
+
+
+
+
+function Aportacion() {
+  
+   let [num,setNum] = useState(0)
+  function manexadorAport(evento) {
+   setNum( parseFloat (evento.target.value))
+   if (/^[0-9]*\.?[0-9]{0,2}$/.test ((evento.target.value)))
+   {setNum(evento.target.value)} else {
+    
+     setNum(0);}
+
+    }
+
+    return (
+      
+
+      <div className="aportacion">
+        <img src={customer} alt="user" />
+       <input type="text"/>
+      <label>
+        Aportación:
+      <input type='text' value={num} onInput={manexadorAport} ></input>
+      </label>
+      <label>
+        Saldo:
+        <Saldo/>
+      </label>
+      
+      </div>
+     
+    )
+   
+}
+ export default Aportacion
+
+ 
