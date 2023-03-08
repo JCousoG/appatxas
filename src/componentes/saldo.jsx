a36154efeac9fa06dd316929e8bc274b5cd1514e
Couso <jcousog@hotmail.com>
2023-03-08 20:44:00 +0100

    valor 0 correxido

--- src/componentes/saldo.jsx
+++ src/componentes/saldo.jsx
@@ -12,7 +12,7 @@ function Saldo(props) {
  return (
     <>
     {props.diferencia < 0 && <span className="debe"><img src={imagedebe} alt="debe"/>  {props.diferencia}<img src={eurodebe} alt="eurodebe"/></span>}
-    {props.diferencia === 0 && <span className="pagou"><img src={imagepagou} alt="pagou"/> {props.diferencia}<img src={europagou} alt="europagou"/></span>}
+    {props.diferencia == 0 && <span className="pagou"><img src={imagepagou} alt="pagou"/> {props.diferencia}<img src={europagou} alt="europagou"/></span>}
     {props.diferencia > 0 && <span className="pagoudemais"><img src={imagesobra} alt="pagoudemais"/> {props.diferencia}<img src={pagoudemais} alt="pagoudemais"/></span>}
     </>
  )
